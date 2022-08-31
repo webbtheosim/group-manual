7bb25d52538294473cffd4341f442630818d648a
webbtheosim <98232803+webbtheosim@users.noreply.github.com>
2022-08-31 12:40:39 -0400

    removed myst_nb

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,6 @@ release = '0.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "myst_nb",
     "sphinx.ext.intersphinx",
     "sphinx.ext.githubpages",
 ]
